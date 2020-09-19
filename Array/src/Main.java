f0f96ffbff29528d5b2646fec904bf47c50e8292
BurbonSkill <41130909+BurbonSkill@users.noreply.github.com>
2020-09-19 12:51:20 +0300

    asd

--- Array/src/Main.java
+++ Array/src/Main.java
@@ -1,6 +1,20 @@
-public class Main {
+public class ex17 {
 
-    public static void main(String[] args) {
-        System.out.println("hello world!");
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        int tripMiles=1;
+        int tripGallons;
+        System.out.printf("Enter the miles driven and gallons used to obtain miles per gallon.%n Or enter 0 to stop running program.%n");
+        while(tripMiles!=0) {
+            tripMiles=input.nextInt();
+            if(tripMiles!=0) {
+                tripGallons=input.nextInt();
+                System.out.printf("Miles per gallon: %.2f %nEnter new values or 0 to stop!%n",(double)tripMiles/tripGallons);
+            }
+            else {
+                break;
+            }
+        }
     }
-}
